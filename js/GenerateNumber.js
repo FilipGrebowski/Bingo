179dd639f889e18f41acf3f5be53fcd69c9d3614
Filip Grebowski <grebowskifilip@gmail.com>
2018-03-03 16:11:23 +0000

    Sorted files into folders.

---
+++ js/GenerateNumber.js
@@ -0,0 +1,29 @@
+// Holds the current generated numbers without repeats.
+var exists = [];
+
+// Runs an anonymous function upon clicking the start button.
+document.querySelector('.start-button').addEventListener('click', function() {
+    
+    // Generates and holds a random number between 1-75.
+    var randomNumber = Math.floor(Math.random() * 75) + 1;
+    
+    document.querySelector('.start-button').style.display = 'none';
+    document.querySelector('.no-button').style.display = 'initial';
+    document.querySelector('.yes-button').style.display = 'initial';
+    
+    var obj = document.createElement('div');
+    obj.id = "display-number";
+    document.querySelector('.button-div').appendChild(obj);
+    
+//    while (exists.length !== 75) {
+//        if (exists.includes(randomNumber)) {
+//            void 0;
+//        }
+//        else {
+//            document.querySelector('.middle-button-box').innerHTML = randomNumber;
+//            exists.push(randomNumber);
+//            console.log(exists);
+//        }
+//    }
+    
+});
